d16b2a9ec05257471061678c0e92135391cd67cc
ricchis <126465308+ricchis@users.noreply.github.com>
2023-03-11 23:14:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const textElement = document.getElementById("text");
+const copyButton = document.getElementById("copy");
+
+const copyText = (e) => {
+  window.getSelection().selectAllChildren(textElement);
+  document.execCommand("copy");
+  e.target.setAttribute("tooltip", "Copied min=$10 ✅");
+};
+
+const resetTooltip = (e) => {
+  e.target.setAttribute("tooltip", "Copy to clipboard");
+};
+
+copyButton.addEventListener("click", (e) => copyText(e));
+copyButton.addEventListener("mouseover", (e) => resetTooltip(e));
